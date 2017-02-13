ac46c04a6a59f35c549756bdcea5daa15aa6463c
bxtkezhan <bxtkezhan@qq.com>
2017-02-13 20:26:25 +0800

    修改文档网页

--- bar4py/__init__.py
+++
@@ -1,6 +0,0 @@
-from bar4py.marker import Marker, createMarker
-from bar4py.dictionary import Dictionary
-from bar4py.markerdetector import MarkerDetector
-from bar4py.cameraparameters import CameraParameters
-from bar4py.web import createWebPlayer
-from bar4py.version import __version__
--- bar4py/resources/static/animate/animate.js
+++
@@ -1,8 +0,0 @@
-var RotateTag = 0;
-function animate(id, model) {
-    if (id == '701') {
-        model.rotateX(Math.PI/2);
-        model.rotateY(RotateTag);
-        RotateTag += 0.1;
-	}
-}
